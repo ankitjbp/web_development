791785432ca16e968fe611b97263da4638169d53
ankitjbp <ankit.jbp@gmail.com>
2025-01-21 08:57:19 +0530

    database class 2

---
+++ Database/schema.sql
@@ -0,0 +1,14 @@
+mysql -u root -p
+
+show databases;
+
+create database online_class;
+
+use online_class;
+
+create table student  
+(id integer(3),name varchar(100), age integer(2)stream varchar(50) );
+
+insert into student values(1,'Dwarika',40,'professional');
+
+select * from student;
