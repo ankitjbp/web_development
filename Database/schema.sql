0572fce22e729140cca9531b329ce9993801fb61
ankitjbp <ankit.jbp@gmail.com>
2025-01-22 08:27:07 +0530

    database class - 3

--- Database/schema.sql
+++ Database/schema.sql
@@ -9,6 +9,10 @@ use online_class;
 create table student  
 (id integer(3),name varchar(100), age integer(2)stream varchar(50) );
 
+show tables;
+
+desc student;
+
 insert into student values(1,'Dwarika',40,'professional');
 
 select * from student;
---
+++ Database/schema2.sql
@@ -0,0 +1,12 @@
+create table subject
+
+(
+id integer(3), 
+name varchar(40),
+display varchar(1),
+create_date datetime
+);
+
+insert into subject values (1,'JavaScript','Y','2025-01-22 08:00:00'); 
+
+insert into subject(id,name,create_date) values (2,'Database','2025-01-22 08:15:00');
