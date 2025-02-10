3cbda51e051252b5f659372343c5d0e17797cf70
Rahulkumarnamdeo <rahulkumarnamdeo23@gmail.com>
2025-02-10 05:31:17 -0800

    Update schema2.sql
    create class table

--- Database/schema2.sql
+++ Database/schema2.sql
@@ -10,3 +10,12 @@ create_date datetime
 insert into subject values (1,'JavaScript','Y','2025-01-22 08:00:00'); 
 
 insert into subject(id,name,create_date) values (2,'Database','2025-01-22 08:15:00');
+
+create table class
+(
+id integer(3), 
+name varchar(40),
+sub_name varchar(40),
+display varchar(1),
+create_date datetime
+);
