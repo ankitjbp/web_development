cb4850e4d537cfab89b549a1492fa75fd0ea09e8
ankitjbp <ankit.jbp@gmail.com>
2025-02-17 08:14:58 +0530

    web technologies - PHP introduction

---
+++ WebTechnologies/PHP/17022025/php_project/index.php
@@ -0,0 +1,27 @@
+
+<html>
+<head>
+<title>For WebPage</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+
+<?php 
+echo "Welcome<br/>";
+?>
+<a href="outer_page.html">Outer Page</a> |
+<a href="external_css.html">External  CSS</a> |
+<div id="top_banner">
+For top Banner
+<img src="image.png" style="height:100px;"/>
+
+</div>
+<div  id="sidebar">
+For Left Sidebar
+</div>
+<div id="content">
+For Content
+</div>
+
+</body>
+</html
