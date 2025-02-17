fb2f4b884b2497cba4924776d921236564e74ecf
Rahulkumarnamdeo <rahulkumarnamdeo23@gmail.com>
2025-02-17 05:29:50 -0800

    Update schema.sql- create database school and create 2 tables result and student

--- Database/schema.sql
+++ Database/schema.sql
@@ -16,3 +16,18 @@ desc student;
 insert into student values(1,'Dwarika',40,'professional');
 
 select * from student;
+
+
+create database school;
+create table result 
+(id integer(3),name varchar(100), age integer(2), Markes integer(3),stream varchar(50));
+insert into result values(1,'Dwarika',40,'85''Computer Science');
+insert into result values(2,'Naveen',42,'80''Maths Science');
+
+insert into result values(3,'Rahul',30,'70''Commerce Computer');
+
+create table student 
+(id integer(3),name varchar(100),age integer(2),result_id integer(3),address varchar(50));
+insert into result values(1,'Dwarika',40,1,'Ghamapur');
+insert into result values(2,'Naveen',42,2,'Rampur');
+insert into result values(3,'Rahul',30,3,'Civil Lines');
